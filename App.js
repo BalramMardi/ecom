9652aeed5613f3764c0fe3ae99a35c215a4a844d
BalramMardi <120470787+BalramMardi@users.noreply.github.com>
2023-12-19 20:36:07 +0530

    initial update

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello, React Native! starting date</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
+
+export default App;
